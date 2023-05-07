1544330a1b8030fc24fd71678aaa28ad977672e4
Donhyeong Lee <dhm_w@icloud.com>
2023-05-08 08:28:39 +0900

    Initial Commit

---
+++ Volume Counter/Volume_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Volume_CounterApp.swift
+//  Volume Counter
+//
+//  Created by 이돈형 on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct Volume_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
